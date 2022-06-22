04fe11ce60142198776671b0827f87844d72c0e2
Yash Ori <yash.ori498@gmail.com>
2022-06-22 21:53:26 +0400

    fixed link model

--- server/model/Link.js
+++ server/model/Link.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
-const linkSchema = new mongoose.Schema({
-    target: {
-        type: String,
-        required: true,
-        createdAt: new Date(),
-        unique: true,
+const linkSchema = new mongoose.Schema(
+    {
+        target: {
+            type: String,
+            required: true,
+            unique: true,
+        },
     },
-});
+    { timestamps: true }
+);
 
 export default mongoose.model("Link", linkSchema);
