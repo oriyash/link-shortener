52d212274d4e6aac179fd679511280c0da1438f3
Yash Ori <yash.ori498@gmail.com>
2022-06-22 22:02:42 +0400

    added materialize dep

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "materialize-css/dist/css/materialize.css";
 
 const app = createApp(App);
 
 app.mount("#app");
+import "materialize-css/dist/js/materialize.js";
